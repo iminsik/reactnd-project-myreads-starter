e6c408745a6ddb9fc3fceac858123fcbb85c1946
Insik Cho <iminsik@gmail.com>
2017-08-12 18:57:53 -0700

    fetch books from service and refactor

---
+++ src/ListBooks.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class ListBooks extends React.Component {
+  render() {
+    return (
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">{this.props.shelf}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                {
+                  this.props.books.map((book, idx) => ( 
+                    <li key={idx}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
+                          <div className="book-shelf-changer">
+                            <select>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors.join(', ')}</div>
+                      </div>
+                    </li>
+                  ))
+                }
+                </ol>
+              </div>
+            </div>
+    )
+  }
+}
+
+export default ListBooks
