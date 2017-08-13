0f8ec102c82adcdfa58542525a71c30eafe6ba66
Insik Cho <iminsik@gmail.com>
2017-08-12 23:03:00 -0700

    populate shelf info

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -11,7 +11,7 @@ class ListBooks extends React.Component {
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
                     <div className="book-shelf-changer">
-                      <select>
+                      <select defaultValue={book.shelf}>
                         <option value="none" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
