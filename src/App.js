443458d3188bbdead42bd358cc3af5e5ab6cffc3
Insik Cho <iminsik@gmail.com>
2017-08-12 23:18:09 -0700

    refactor parts of App.js to Books.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
-import ListBooks from './ListBooks'
+import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
-import * as BooksAPI from './BooksAPI'
+import Books from './Books'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    books: []
-  }
-
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({books})
-    })
-  }
-
   render() {
-    const shelfCategories = [{category: 'read', title: 'Read'}, {category: 'currentlyReading', title: 'Currently Reading'}, {category: 'wantToRead', title: 'Want To Read'}]
-
     return (
       <div className="app">
         <Route exact path='/search' render={() => (
@@ -26,29 +13,7 @@ class BooksApp extends React.Component {
         )} />
 
         <Route exact path='/' render={() => (
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
-              <div className="list-books-content">
-                <div>
-                {
-                  shelfCategories.map((shelf, idx) => (
-                    <div key={idx} className="bookshelf">
-                      <h2 className="bookshelf-title">{shelf.title}</h2>
-                      <div className="bookshelf-books">
-                      <ListBooks
-                        books={this.state.books.filter(book => book.shelf === shelf.category)} />
-                      </div>
-                    </div>
-                  ))
-                }
-                </div>
-              </div>
-              <div className="open-search">
-                <Link to="/search">Add a book</Link>
-              </div>
-            </div>
+            <Books />
         )} />
       </div>
     )
